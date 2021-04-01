005568e64bcd014dbdd86e64dc6d7988cf6e0e0b
SjoerdKrabben <sjoerdkrabben@gmail.com>
2021-04-01 19:28:14 +0200

    Initial commit

---
+++ src/com/HAN/deeltijd/leerjaar1/Main.java
@@ -0,0 +1,8 @@
+package com.HAN.deeltijd.leerjaar1;
+// Als het goed is heb je hierin de Processing library ook mee
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
