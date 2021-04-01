0cdfa20a422e11497e72def31a4e9a81963e4ab8
Thomas <Thomasvdo@hotmail.com>
2021-04-01 19:58:53 +0200

    PUSH DAN

--- src/com/HAN/deeltijd/leerjaar1/Main.java
+++ src/com/HAN/deeltijd/leerjaar1/Main.java
@@ -1,5 +1,7 @@
 package com.HAN.deeltijd.leerjaar1;
 // Als het goed is heb je hierin de Processing library ook mee
+
+// Moet hier niet import processing.core.papplet?
 public class Main {
 
     public static void main(String[] args) {
